758b654e4c9286b8373e1348834ee149a79a3f75
Ropo Olatujoye <ropo@nairabox.com>
2020-12-28 08:17:56 +0100

    update remove duplicate from sorted array solutions

--- longest-substring.js
+++ longest-substring.js
@@ -2,20 +2,20 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  let maxLength = 0;
+var lengthOfLongestSubstring = function (s) {
   let set = new Set();
+  let maxLength = 0;
   let left = 0;
   let right = 0;
-  
+
   while (right < s.length) {
-      if (!set.has(s[right])) {
-          set.add(s[right++])
-          maxLength = Math.max(set.size, maxLength);
-      } else {
-          set.delete(s[left++]);
-      }
+    if (!set.has(s[right])) {
+      set.add(s[right++]);
+      maxLength = Math.max(set.size, maxLength);
+    } else {
+      set.delete(s[left++]);
+    }
   }
 
-return maxLength
-}
+  return maxLength;
+};
--- remove-duplicate-from-sorted-array.js
+++ remove-duplicate-from-sorted-array.js
@@ -1,4 +1,6 @@
 var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i + 1]) {
       nums.splice(i, 1);
@@ -7,4 +9,18 @@ var removeDuplicates = function (nums) {
   }
 
   return nums.length;
+};
+
+const removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      nums[++i] = nums[j];
+    }
+  }
+
+  return i + 1;
 };
