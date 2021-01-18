3ad9c2bf7115e0c43d631ece75c9f321c5da22a7
Ropo Olatujoye <ropo@nairabox.com>
2021-01-18 22:22:20 +0100

    add more solution

--- strStr.js
+++ strStr.js
@@ -23,3 +23,46 @@ var strStr = function (haystack, needle) {
 
   return -1;
 };
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ 
+ Question: https://leetcode.com/problems/implement-strstr
+ 
+ Approach: Two Pointers: Linear Time Window Slice
+ Time Complexity: O((m-n)n)
+ Space Complexity: O(1)
+ */
+var strStr = function (haystack, needle) {
+  const m = haystack.length;
+  const n = needle.length;
+  let pm = 0;
+
+  if (n === 0) return 0;
+  if (n > m) return -1;
+
+  while (pm <= m - n) {
+    // find the position of the first needle's character in haystack
+    while (pm <= m - n && haystack.charAt(pm) !== needle.charAt(0)) ++pm;
+
+    let currentLength = 0,
+      pn = 0;
+
+    // compute the max match string
+    while (pn < n && pm < m && haystack.charAt(pm) === needle.charAt(pn)) {
+      ++pm;
+      ++pn;
+      ++currentLength;
+    }
+
+    // if needle length is found, return the starting position
+    if (currentLength === n) return pm - n;
+
+    // backtracking
+    pm = pm - currentLength + 1;
+  }
+
+  return -1;
+};
