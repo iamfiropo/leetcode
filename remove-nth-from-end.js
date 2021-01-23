8ecc048ed5fc5642ca59e339af52ac95eafdfd87
Ropo Olatujoye <ropo@nairabox.com>
2021-01-23 22:22:24 +0100

    add more solution to remove nth from end

--- remove-nth-from-end.js
+++ remove-nth-from-end.js
@@ -11,6 +11,7 @@
  * @return {ListNode}
  
  Question: https://leetcode.com/problems/remove-nth-node-from-end-of-list
+
  Approach: One Pass
  Time Complexity: O(n)
  Space Complexity: O(1)
@@ -23,11 +24,49 @@ var removeNthFromEnd = function (head, n) {
   while (fast.next !== null) {
     fast = fast.next;
 
+    // keep the slow pointer unattended to until there is an n nodes gap between slow and fast pointer
     if (n-- <= 0) {
       slow = slow.next;
     }
   }
   slow.next = slow.next.next;
+
+  return dummyHead.next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ 
+ Approach: Two Pass
+ Time Complexity: O(n)
+ Space Complexity: O(1)
+ */
+var removeNthFromEnd = function (head, n) {
+  const dummyHead = new ListNode(0, head);
+  let length = 0;
+  let first = head;
+
+  while (first !== null) {
+    ++length;
+    first = first.next;
+  }
+
+  length -= n;
+  first = dummyHead;
+  console.log({ first });
+
+  while (length-- > 0) first = first.next;
+
+  first.next = first.next.next;
 
   return dummyHead.next;
 };
