13eb3c1b3e15bea83db5a456264694e3ab146114
Ropo Olatujoye <ropo@nairabox.com>
2021-01-26 11:32:35 +0100

    add recursive solution

--- merge-two-sorted-list.js
+++ merge-two-sorted-list.js
@@ -36,3 +36,32 @@ var mergeTwoLists = function (l1, l2) {
   prev.next = l1 === null ? l2 : l1;
   return dummyHead.next;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ 
+ Approach: Recursion
+ Time Complexity: O(n + m)
+ Space Complexity: O(n + m)
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
