0b350ba1b8a83b1f6d9e27e7dd6ddc9d5608373e
Ropo Olatujoye <ropo@nairabox.com>
2021-02-23 15:06:02 +0100

    add climb stairs solution

--- climb-stair.js
+++ climb-stair.js
@@ -1,5 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number}
+ 
+ Question: https://leetcode.com/problems/climbing-stairs
+ 
+ Approach: Recursion with memoization
+ 
+ Time Complexity: O(n)
+ Space Complexity: O(n)
+ */
+
+function climbStairs(n) {
+  function stairs(n, cache) {
+    // Edge Case
+    if (n < 0) return 0;
+
+    // Base Case
+    if (n === 0) return 1;
+
+    // check the cache for the result of previous operations
+    if (cache[n]) return cache[n];
+
+    // Recursive case
+    return (cache[n] = stairs(n - 1, cache) + stairs(n - 2, cache));
+  }
+
+  return stairs(n, {});
+}
+
 var climbStairs = function (n) {
-  const cache = { "0": 0, "1": 1, "2": 2 };
+  const cache = { 0: 0, 1: 1, 2: 2 };
 
   function stairs(n) {
     if (n in cache) {
