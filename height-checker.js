4fd325b42de4b04f7df59e2a5a75a600a7860b7b
Ropo Olatujoye <iamfiropo@gmail.com>
2022-05-05 05:56:37 +0100

    solution of height checker

---
+++ height-checker.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ * 
+ *  * Question: https://leetcode.com/problems/height-checker
+
+ Approach: One pass
+ Time Complexity: O(n)
+ Space Complexity: O(n)
+ */
+ var heightChecker = function(heights) {
+	let count = 0;
+	let expectedIdx = 0;
+	
+	// sort a copy of the input array
+	const expectedHgts = [...heights].sort((a, b) => a - b);
+	
+	for (const height of heights) {
+		if (height !== expectedHgts[expectedIdx++]) count++
+	}
+	
+	return count;
+};
